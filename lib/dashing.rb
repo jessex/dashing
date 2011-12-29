475d40759cbfa5b79f2c83ddaa2a4f081374d511
jessex <essexjosh@gmail.com>
2011-12-29 15:17:28 -0500

    Beginning to add dashboard infrastructure

--- lib/dashing.rb
+++ lib/dashing.rb
@@ -4,9 +4,17 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'dashing')
 
 require 'configuration'
 require 'dashboard'
+require 'sinatra'
 
 Dir[File.join(File.dirname(__FILE__), 'dashing', 'dash')].each { |file| require file }
+
+set :views, settings.root + '/../views'
+set :public_folder, File.dirname(__FILE__) + '/../public'
 
 configuration = Configuration.load_configuration
 dashboard = Dashboard::Board.new configuration
 Dashboard.validate_layout dashboard
+
+get '/dashboard' do
+  erb :dashboard, :locals => { :board => dashboard }
+end
